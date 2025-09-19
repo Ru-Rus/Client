4612f402c79af2371adbc4dc8c809253ac12e258
RMMarquillero <rmmarquillero@sscgi.com>
2025-09-19 10:41:27 +0800

    (Error Page - JSX) Refactor Error component to improve error handling and user feedback

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,32 @@
 import { Link, useRouteError } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/ErrorPage'
+import img from '../assets/images/not-found.svg'
+
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+
+  if(error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found"></img>
+          <h3>
+            Oops! The page you're looking for does not exist.
+          </h3>
+          <p>
+            we could not find the page you were looking for.
+          </p>
+          <Link to="/dashboard">Go to Home</Link>
+        </div>
+      </Wrapper>
+    )
+  }
   return (
-    <div>
-      <h1>Error</h1>
-      <Link to="/">Go to Home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3> Something Went Wrong</h3>
+      </div>
+    </Wrapper>
   );
 };
 
