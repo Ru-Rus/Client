1becdc16df746a18de51f7da77d9eff9a07bdd38
RMMarquillero <rmmarquillero@sscgi.com>
2025-09-19 13:54:32 +0800

    (FormRow Component) Add FormRow component and refactor Register page to utilize it for form inputs

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import Logo from '../assets/components/logo';
+import { FormRow } from '../assets/components';
 
 const Register = () => {
   return (
@@ -11,10 +12,12 @@ const Register = () => {
         <h4>
           Register
         </h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'> Name</label>
-          <input type='text' id='name' className='form-input' name='name' required defaultValue="Kaizer"/>
-        </div>
+          <FormRow type='text' name='name' defaultValue='Kaizer' />
+          <FormRow type='text' name='lastName' labelText='last name' defaultValue='hell' />
+          <FormRow type='text' name='location' defaultValue='Makati' />
+          <FormRow type='email' name='email' defaultValue='email@gmail.com'/>
+          <FormRow type='password' name='password' defaultValue='P@$$w0rd'/>
+
         <button type='submit' className='btn btn-block'>Submit</button>
         <p> Already a member ?
           <Link to='/login' className='member-btn'> Login</Link>
