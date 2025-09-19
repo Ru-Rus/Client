2a280b9508894575d521deb5ec62f03fdd9ef37d
RMMarquillero <rmmarquillero@sscgi.com>
2025-09-19 15:54:31 +0800

    (Login Page) Integrate FormRow components for email and password inputs in Login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
-import { Logo } from '../assets/components';
+import { FormRow, Logo } from '../assets/components';
 
 const Login = () => {
   return (
@@ -9,7 +9,15 @@ const Login = () => {
       <form className='form'>
         <Logo />
         <h4>Login</h4>
-    
+        <FormRow type='email' name='email' defaultValue='sample@gmail.com'/>
+        <FormRow type='password' name='password' defaultValue='P@$$w0rd'/>
+        <button type ='submit' className='btn btn-block'>Submit</button>
+        <button type ='button' className='btn btn-block'>Explore the App</button>
+        <a> 
+          Not a member yet? 
+          <Link to='/register'>Register</Link>
+        </a>
+
       </form>
     </Wrapper>
   )
