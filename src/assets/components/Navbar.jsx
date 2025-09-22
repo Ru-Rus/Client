1ebe89f0e3d1fe159b25b68b20e2870a3f104274
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-09-22 09:01:42 +0800

    (Dashboard Structure) Add Sidebar and Navbar components; refactor DashboardLayout to include them

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Wrapper from '../wrappers/Navbar'
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <h1>Navbar</h1>
+    </Wrapper>
+  )
+}
+
+export default Navbar
---
+++ src/assets/components/SmallSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Wrapper from '../wrappers/SmallSidebar'
+const SmallSidebar = () => {
+  return (
+    <Wrapper>
+      <h1>Small Sidebar</h1>
+    </Wrapper>
+  )
+}
+
+export default SmallSidebar
