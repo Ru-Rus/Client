f4b30981bf479ca12b8515d3ad771df186946ec7
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-09-22 16:53:28 +0800

    (Morgan and dotENV)

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
+import * as dotenv from 'dotenv'
+dotenv.config();
+import morgan from 'morgan'
 import express from 'express';
+
+
+
 const app = express();
+
+app.use(express.json());
+
+
+
+if (process.env.NODE_ENV ==='development'){
+    app.use(morgan('dev'));
+}
+
 
 
 app.get('/', (req,res) => {
-    res.send("is the path i am taking the right path ? ")
+    res.send("Hello is the path i am taking the right path ? ")
 }); 
 
-app.listen(5100, () => {
-    console.log ("what am i doing, SEND HELP!!")
+app.post('/',(req,res) =>{
+    console.log(req);
+    res.json({message:'Received Payment for this illegal transactions',data: req.body})
+});
+
+const port = process.env.PORT || 5100
+
+app.listen(port, () => {
+    console.log (`Running as ${port} what am i doing, SEND HELP!!`) 
 
 })
