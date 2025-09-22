e27081646e853e808d3bbcbfb725676b75180ed2
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-09-22 09:41:39 +0800

    (Dashboard Context) Refactor DashboardLayout to implement context for managing sidebar and theme state; update SmallSidebar to consume context data

--- src/assets/components/SmallSidebar.jsx
+++ src/assets/components/SmallSidebar.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Wrapper from '../wrappers/SmallSidebar'
+import { useDashboardContext } from '../../pages/DashboardLayout'
+
 const SmallSidebar = () => {
+   const data = useDashboardContext();
+   console.log(data);
+
+
+
+
   return (
     <Wrapper>
       <h1>Small Sidebar</h1>
